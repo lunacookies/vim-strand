ffeec2756dc19671776a5b2aefe6aa7940c87df3
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-12-02 20:23:04 +1100

    Expand tilde home dir notation in plugin_dir field in config

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,40 @@ use std::{
     path::{Path, PathBuf},
 };
 
+fn get_home_dir() -> PathBuf {
+    use std::process;
+
+    match dirs::home_dir() {
+        Some(dir) => dir,
+        None => {
+            eprintln!("Error: could not locate home directory -- exiting.");
+            process::exit(1);
+        }
+    }
+}
+
+fn expand_path(path: &Path) -> PathBuf {
+    use std::path::Component;
+
+    if path.starts_with("~") {
+        let mut components: Vec<_> = path.components().collect();
+        let home_dir = get_home_dir().into_os_string();
+
+        // Remove the tilde and add in its place the home directory.
+        components.remove(0);
+        components.insert(0, Component::Normal(&home_dir));
+
+        // Join the components back into a single unified PathBuf.
+        let mut path = PathBuf::new();
+        components.iter().for_each(|segment| path.push(segment));
+
+        path
+    } else {
+        path.into()
+    }
+}
+
+// git_ref can be a branch name, tag name, or commit hash.
 #[derive(Deserialize)]
 pub struct GitHubPlugin {
     user: String,
@@ -71,7 +105,10 @@ pub async fn get_config(config_file: &Path) -> Result<Config> {
     use async_std::fs;
 
     let config = fs::read_to_string(config_file).await?;
-    Ok(yaml::from_str(&config)?)
+    let mut config: Config = yaml::from_str(&config)?;
+    config.plugin_dir = expand_path(&config.plugin_dir);
+
+    Ok(config)
 }
 
 fn decompress_tar_gz(bytes: &[u8], path: &Path) -> Result<()> {
@@ -98,4 +135,29 @@ pub async fn install_plugins(plugins: Vec<Plugin>, dir: PathBuf) -> Result<()> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expand_path() {
+        let home_dir = get_home_dir();
+
+        assert_eq!(
+            expand_path(Path::new("~/foo.txt")),
+            home_dir.join("foo.txt")
+        );
+
+        assert_eq!(
+            expand_path(Path::new("/home/person/foo.txt")),
+            PathBuf::from("/home/person/foo.txt")
+        );
+
+        assert_eq!(
+            expand_path(Path::new("~/bar/baz/quux/foo.txt")),
+            home_dir.join("bar/baz/quux/foo.txt")
+        );
+    }
 }
