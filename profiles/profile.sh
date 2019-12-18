747382b2a68628b96c70a9b49392f41cdc727656
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-12-18 17:07:05 +1100

    Fix some typos and improve consistency in profile

--- profiles/profile.sh
+++ profiles/profile.sh
@@ -19,56 +19,55 @@ mkdir "$vim_config"
 
 # Create a strand config with a random set of plugins, one of which is specified
 # for vim-plug in run_profile.vim.
-echo "
+echo '
 ---
 plugin_dir: ~/.vim/pack/strand/start
 
 plugins:
-  - Git: github@PeterRincker/vim-searchlight
-  - Git: github@cakebaker/scss-syntax.vim
-  - Git: github@cespare/vim-toml
-  - Git: github@christoomey/vim-tmux-navigator
-  - Git: github@cocopon/inspecthi.vim
-  - Git: github@hail2u/vim-css3-syntax
-  - Git: github@jonathanfilip/vim-lucius
-  - Git: github@junegunn/fzf.vim
-  - Git: github@junegunn/vim-easy-align
-  - Git: github@justinmk/vim-dirvish
-  - Git: github@kana/vim-textobj-user
-  - Git: github@kh3phr3n/python-syntax
-  - Git: github@lifepillar/vim-colortemplate
-  - Git: github@lifepillar/vim-mucomplete
-  - Git: github@othree/html5.vim
-  - Git: github@pangloss/vim-javascript
-  - Git: github@reedes/vim-textobj-quote
-  - Git: github@romainl/vim-cool
-  - Git: github@romainl/vim-qf
-  - Git: github@rust-lang/rust.vim
-  - Git: github@sgur/vim-editorconfig
-  - Git: github@tmsvg/pear-tree
-  - Git: github@tmux-plugins/vim-tmux
-  - Git: github@tpope/vim-commentary
-  - Git: github@tpope/vim-endwise
-  - Git: github@tpope/vim-fugitive
-  - Git: github@tpope/vim-git
-  - Git: github@tpope/vim-markdown
-  - Git: github@tpope/vim-repeat
-  - Git: github@tpope/vim-surround
-  - Git: github@tpope/vim-unimpaired
-  - Git: github@wellle/targets.vim
-" > "$strand_config"
+  - Git: PeterRincker/vim-searchlight
+  - Git: cakebaker/scss-syntax.vim
+  - Git: cespare/vim-toml
+  - Git: christoomey/vim-tmux-navigator
+  - Git: cocopon/inspecthi.vim
+  - Git: hail2u/vim-css3-syntax
+  - Git: jonathanfilip/vim-lucius
+  - Git: junegunn/fzf.vim
+  - Git: junegunn/vim-easy-align
+  - Git: justinmk/vim-dirvish
+  - Git: kana/vim-textobj-user
+  - Git: kh3phr3n/python-syntax
+  - Git: lifepillar/vim-colortemplate
+  - Git: lifepillar/vim-mucomplete
+  - Git: othree/html5.vim
+  - Git: pangloss/vim-javascript
+  - Git: reedes/vim-textobj-quote
+  - Git: romainl/vim-cool
+  - Git: romainl/vim-qf
+  - Git: rust-lang/rust.vim
+  - Git: sgur/vim-editorconfig
+  - Git: tmsvg/pear-tree
+  - Git: tmux-plugins/vim-tmux
+  - Git: tpope/vim-commentary
+  - Git: tpope/vim-endwise
+  - Git: tpope/vim-fugitive
+  - Git: tpope/vim-git
+  - Git: tpope/vim-markdown
+  - Git: tpope/vim-repeat
+  - Git: tpope/vim-surround
+  - Git: tpope/vim-unimpaired
+  - Git: wellle/targets.vim
+' > "$strand_config"
 
-# Install vim- provider:plug before running the profil
+# Install vim-plug before running the profile
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Run the profile harness without sourcing the user’s vimrc (but still keeping
-# plugins and syntax highlighting)
-vim -u NORC "+source profile_harness.vim"
+# Run the profile harness without sourcing the user’s vimrc
+vim -Nu NORC '+source profile_harness.vim'
 
 # Output the log to the user
 cat profile_log
 
 # Restore the original config files
 [ -e "$orig_strand_config" ] && mv "$orig_strand_config" "$strand_config"
-[ -e "$orig_vim_config" ] && rm -r "$vim_config" && mv "$orig_vim_config" "$vim_config"
+[ -e "$orig_vim_config" ] && rm -rf "$vim_config" && mv "$orig_vim_config" "$vim_config"
